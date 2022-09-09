5b61b36d739460cfaf10b0a1f4e4b959c1164cce
ambroan1 <112683392+ambroan1@users.noreply.github.com>
2022-09-09 09:22:08 +0200

    I commit merge

--- src/merge.py
+++ src/merge.py
@@ -11,12 +11,26 @@ def merge(x: list[int], y: list[int]) -> list[int]:
     >>> merge([1, 2, 4, 6], [1, 3, 4, 5])
     [1, 1, 2, 3, 4, 4, 5, 6]
     """
+
+    #if len(x) == 0:
+    #    return y
+    #if len(y) == 0:
+    #    return x
+
     i, j = 0, 0
     z = []  # a new list to copy elements into
-    # FIXME: fill out the loop so you merge the lists
-    # until one of them is empty
+
     while i < len(x) and j < len(y):
-        break  # FIXME: you shouldn't just break here
-    # At least one of the lists is empty now. Copy the
-    # remainder of the other into z.
+
+        if x[i] < y[j]:
+            z.append(x[i])
+            i += 1
+        else:
+            z.append(y[j])
+            j += 1
+
+    if i >= len(x):
+        z = z + y[j:]
+    elif j >= len(y):
+        z = z + x[i:]
     return z
